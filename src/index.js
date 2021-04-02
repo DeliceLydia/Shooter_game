c9b025aaa6b762fa8b6cff0f8a42564683be6d0e
delicelydia <Delydia84@gmail.com>
2021-04-02 17:11:34 +0200

    imported initializeGame function in the index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import initializeGame from '../views/initialize';
+import shooter_game from '../src/js/Game';
+
+
+const user = JSON.parse(localStorage.getItem('playerName'));
+if (!user) {
+  window.addEventListener('DOMContentLoaded', initializeGame);
+} else {
+  shooter_game();
+}
 
